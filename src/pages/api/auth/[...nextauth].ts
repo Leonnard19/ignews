ec30a3013025d57bd27b9983eabc6556e37a0938
Leonardo <srleon19@hotmail.com>
2022-11-30 15:43:52 -0300

    implemented faunadb

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,7 +1,11 @@
+import { query as q } from 'faunadb';
+
 import NextAuth from 'next-auth';
 import GithubProvider from 'next-auth/providers/github';
 
-export const authOptions = {
+import { fauna } from '../../../services/fauna';
+
+export default NextAuth({
   // Configure one or more authentication providers
   providers: [
     GithubProvider({
@@ -10,6 +14,24 @@ export const authOptions = {
     }),
     // ...add more providers here
   ],
-};
 
-export default NextAuth(authOptions);
+  callbacks: {
+    async signIn(params) {
+      const { email } = params.user;
+
+      try {
+        await fauna.query(
+          q.If(
+            q.Not(q.Exists(q.Match(q.Index('user_by_email'), q.Casefold(email)))),
+            q.Create(q.Collection('users'), { data: { email } }),
+            q.Get(q.Match(q.Index('user_by_email'), q.Casefold(email)))
+          )
+        );
+
+        return true;
+      } catch {
+        return false;
+      }
+    },
+  },
+});
